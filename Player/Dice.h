81fe554ba77cd15d0e2496dcb4a1e3b8fd371cb8
Newplayers12 <nhangiakham@gmail.com>
2021-12-03 22:57:58 +0700

    changing Dice class

--- Player/Dice.h
+++ Player/Dice.h
@@ -1,17 +1,14 @@
 #include <iostream>
+#include <random>
 
 using namespace std;
+random_device rd; // obtain a random number from hardware
+mt19937 gen(rd()); // seed the generator
 
 class Dice{
 public:
-    static int Rand() {
-        return rand();
-    }
-    static int Rand(int n){
-        
-        return rand() % n;
-    }
     static int Rand(int low, int high){
-        return (rand() % (high - low + 1)) + low;
+        uniform_int_distribution<> distr(low, high);
+        return distr(gen);
     }
 };
--- Player/main.cpp
+++ Player/main.cpp
@@ -1,10 +1,16 @@
 #include "Player.h"
 #include "Dice.h"
-
 #include <iostream>
+
+
 using namespace std;
 
 int main(){
-    srand(time(0));
-    
+    int cnt[11] = {0};
+    for(int i = 0; i < 1e5; ++i){
+        cnt[Dice::Rand(1, 10)]++;
+    }
+    for(int i = 1; i <= 10; ++i){
+        cout << cnt[i] << ' ';
+    }
 }
